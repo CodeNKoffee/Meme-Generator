4f8f0fb51af66b3717eedb9c65e3fa6d14c43e5d
Hatem <hatemthedev@gmail.com>
2022-06-06 02:22:14 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import MemeGenerator from './components/MemeGenerator';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
